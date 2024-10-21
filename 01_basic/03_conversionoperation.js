f333f9340965cc6050859d9d99827847edfde3ac
manish21oct <manishsingh21oct@gmail.com>
2024-10-21 10:09:38 +0000

    datatype

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -24,5 +24,40 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**************************************Operation***************************************
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+
+let str1 = "hello "
+let str2 = "manish"
+
+let str3 = str1  +  str2 
+// console.log(str3);
+
+// console.log("1"+ 2 );
+// console.log( 1+ "2");
+// console.log("1" + 2 + "2");
+// console.log(1 + 2 + "2");
+
+// agar string first hai to sabhi ka string me conversion krega agar string last me to phele ka jo conversion hai usko krega 
+
+// console.log((3 + 4) * 5 % 3);
+
+ // console.log(+true);
+ // console.log(+"");   // aisa nhi krte 
+
+ let num1, num2, num3
+ 
+ num1 = num2 = num3 = 2 + 2
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
+
+ //  link to study   https://tc39.es/ecma262/#sec-abstract-operations
---
+++ datatypes.js
@@ -0,0 +1,20 @@
+// Primitive
+
+// 7 type : String ,Number, Boolean , Null , undefined , symbol , bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id= Symbol ('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// reference type (non premitive type )
+
+// Array , objects, Functions 
